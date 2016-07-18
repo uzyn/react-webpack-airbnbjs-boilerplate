2ff442386b400d3bb84ef2644889bd6b8a657050
U-Zyn Chua <chua@uzyn.com>
2016-07-18 17:27:56 +0800

    Airbnb says "relying on function name inference is discouraged)"

--- app/js/component/App.jsx
+++ app/js/component/App.jsx
@@ -1,9 +1,9 @@
 import React from 'react';
 
-const App = () => (
-  <div>
-		Hello world
-  </div>
-);
-
-export default App;
+export default function App() {
+  return (
+    <div>
+      Hello world
+    </div>
+  );
+}
